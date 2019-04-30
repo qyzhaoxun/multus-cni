75ba75e68ca84166b64e7c09da8760e6e4018d0d
xunqyzhao <xunqyzhao@tencent.com>
2019-04-30 15:43:59 +0800

    fix: replace file not equal

--- scripts/install-cni.sh
+++ scripts/install-cni.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env sh
-echo "=====Starting installing cni ========="
-cp /opt/cni/bin/* /host/opt/cni/bin/
+
+set -e
+
+add_replace_files () {
+    local src_dir=$1
+    local dst_dir=$2
+    for f in $(ls ${src_dir}); do
+        if [[ -f ${src_dir}/${f} ]]; then
+            if [[ -f ${dst_dir}/${f} ]]; then
+                if cmp -s ${src_dir}/${f} ${dst_dir}/${f}; then
+                    echo "skip replace equal file ${f}"
+                    continue
+                fi
+                echo "replace file ${f}"
+                cp ${src_dir}/${f} ${dst_dir}/${f}
+                continue
+            fi
+            echo "add file ${f}"
+            cp ${src_dir}/${f} ${dst_dir}/${f}
+            continue
+        fi
+    done
+}
+
 
 echo "=====Starting install cni conf ==========="
-cp /etc/tke-cni-agent-conf/* /host/etc/cni/net.d/
+add_replace_files /etc/tke-cni-agent-conf /host/etc/cni/net.d
+
+echo "=====Starting installing cni ========="
+add_replace_files /opt/cni/bin /host/opt/cni/bin
 
 echo "=====Done==========="
 
