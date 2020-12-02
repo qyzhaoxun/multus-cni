bd0f8d51f2fe44c8bf41da9a13dbd588b517c21b
firmlyzhu <firmlyzhu@tencent.com>
2020-12-03 01:48:46 +0800

    docs: update push.sh and DockerFile and Makefile to build multi arch cni-agent image

--- Makefile
+++ Makefile
@@ -16,6 +16,9 @@ IMAGE ?= $(REGISTRY)/$(IMG_BINARY)
 VERSION ?= $(shell git describe --tags --always --dirty)
 LDFLAGS ?= -X main.version=$(VERSION)
 
+AGENT_TAG := $(shell cat version/next/CNI_Agent)
+AGENT_IMAGE := ccr.ccs.tencentyun.com/tkeimages/tke-cni-agent
+
 # Default to build the Linux binary
 build:
 	GOOS=linux GOARCH=$(GOARCH) CGO_ENABLED=0 go build -o $(BIN_PATH) -ldflags "$(LDFLAGS)" ./
@@ -38,6 +41,18 @@ docker-arm64:
 
 push-arm64:
 	GOARCH=arm64 make push
+
+push-agent:
+	hack/next/push.sh amd64
+
+push-agent-arm64:
+	hack/next/push.sh arm64
+
+buildx-agent: push-agent push-agent-arm64
+	$(eval IMAGE := $(AGENT_IMAGE):$(AGENT_TAG))
+	$(eval IMAGES := $(foreach arch, amd64 arm64, $(AGENT_IMAGE):$(arch)-$(AGENT_TAG)))
+	@echo "===========> push multi-arch image $(AGENT_IMAGE)"
+	@docker buildx imagetools create -t $(IMAGE) $(IMAGES)
 
 clean:
 	rm -rf bin
--- hack/next/push.sh
+++ hack/next/push.sh
@@ -2,9 +2,19 @@
 
 set -e
 
+arch=$1
+if [[ $arch == "" ]]; then
+    arch=amd64
+fi
+BASEIMAGE=amd64/alpine:3.6
+if [[ $arch == "arm64" ]]; then
+    BASEIMAGE=arm64v8/alpine:3.6
+fi
+
 root_path=$(dirname $0)/../..
 cni_tag=$(cat ${root_path}/version/next/CNI_Plugins)
 agent_tag=$(cat ${root_path}/version/next/CNI_Agent)
-echo "build cni-agent ${agent_tag} with cni-plugins ${cni_tag}"
-docker build --build-arg cni_tag=${cni_tag} -t ccr.ccs.tencentyun.com/tkeimages/tke-cni-agent:${agent_tag} -f ${root_path}/scripts/next/Dockerfile .
-docker push ccr.ccs.tencentyun.com/tkeimages/tke-cni-agent:${agent_tag}
+image=ccr.ccs.tencentyun.com/tkeimages/tke-cni-agent
+echo "build cni-agent ${arch}-${agent_tag} with cni-plugins ${cni_tag}"
+docker build --build-arg BASEIMAGE=${BASEIMAGE} --build-arg arch=${arch} --build-arg cni_tag=${cni_tag} -t ${image}:${arch}-${agent_tag} -f ${root_path}/scripts/next/Dockerfile .
+docker push ccr.ccs.tencentyun.com/tkeimages/tke-cni-agent:${arch}-${agent_tag}
